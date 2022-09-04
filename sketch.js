72eace3ec9c4fb58e702a09d091b31bffaeaaf55
Vineethk220 <112795793+Vineethk220@users.noreply.github.com>
2022-09-04 10:15:18 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var box
+function setup() {
+  createCanvas(400,400);
+box = createSprite (200,200,50,50)
+
+}
+
+function draw() 
+{
+  background("black");
+  if (keyDown("right")) {
+    box.x=box.x+5
+  }
+  if (keyDown("left")) {
+    box.x=box.x-5
+  }
+  if (keyDown("down")) {
+    box.y=box.y+5
+  }
+  if (keyDown("up")) {
+    box.y=box.y-5
+  }
+
+
+
+drawSprites()
+}
+
+
+
+
